da5fd53f4319919b77247e4c6c28056a9e6750ff
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-12-31 13:35:48 +0600

    31/12/24

---
+++ vue-project2/src/components/FruitItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    <h1 style="background-color: lightgreen; color: white;">Fruits List</h1>
+<ul style="text-align: left;">
+    <li v-for="(x, index) in fruits" :key="index" >{{ x }}</li>
+</ul>
+
+<ul style="text-align: left;">
+    <li v-bind:key="x" v-for="x in fruits">{{ x }}</li>
+</ul>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                fruits: [
+                'Apple',
+                'Banana',
+                'Mango',
+                'Orange']
+            }
+        },
+    }
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue-project2/src/components/NewYear.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+<h1 v-bind:class="ClassName">Happ new Year 2025</h1>
+<img v-bind:src="url" alt="" width="400px" height="200px">
+<hr>
+<p v-if="productsInStock">
+  in stock
+</p>
+
+<p v-else>
+  not in stock
+</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+         data() {
+            return {
+               ClassName: "head",
+               url: require("@/newyear.jpg"),
+               productsInStock:true
+
+            }
+        },
+        
+    }
+</script>
+
+<style>
+.head{
+    color: green;
+    font-size: 50px;
+    text-align: center;
+    background-color: cornflowerblue;
+
+}
+
+</style>
